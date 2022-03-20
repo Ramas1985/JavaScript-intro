9f7769417550d882266b89478f75b617fb07f4eb
Ramas1985 <ramunasskarnulis@gmail.com>
2022-03-20 14:52:26 +0100

    initial commit

---
+++ js/00-intro.js
@@ -0,0 +1,14 @@
+// single-line comment
+
+/* multi-line comment */
+
+/* Konceptai: 
+
+1) kintamieji- isimena informacija;
+2) salygos sakiniai- palyginimas tarpusavio;
+3) ciklai- darbas su pasikartojancio formato informacija; pasilartpjanciu procesu valdymas;
+4) funkcijos- uzdara logika kuria galima perpanaudoti pagal poreiki;
+5) ivykiai- pvz: uzsikrove puslapis; paspaude mygtuka; pradejo siusti faila ir t.t.;
+6) oop- objektinis programavimas;
+
+*/
