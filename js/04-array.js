2ac205883687808ee59e4c53e9cd4f3e05638426
Ramas1985 <ramunasskarnulis@gmail.com>
2022-03-21 21:07:13 +0100

    array

---
+++ js/04-array.js
@@ -0,0 +1,19 @@
+/* Array */
+
+const marks = [10, 2, 8, 4, 6, 2, 7, 8];
+const size = marks.length;
+
+const firstMark = marks[0];
+const second = marks[1];
+const last = marks[size - 1];
+
+
+console.log(marks);
+console.log(firstMark);
+console.log(second);
+console.log(last)
+
+const dictionary = [`labas`, `rytas`, `sakau`, `tau`];
+
+const paskutinisZodis = dictionary[dictionary.lenght - 1];
+console.log(paskutinisZodis);
