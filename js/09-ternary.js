89ab5912f518413dae702a02bf44d256ebdb591b
Ramas1985 <ramunasskarnulis@gmail.com>
2022-03-24 20:25:57 +0100

    Ternary

---
+++ js/09-ternary.js
@@ -0,0 +1,23 @@
+/*
+Ternary Operator
+
+klausimas? teigiama : neigiama
+
+*/
+
+const arLyja = true;
+
+const sketis = arLyja ? 'pasiimk' : 'nereikia';          // pasiimk /nereikia
+console.log(sketis);
+
+const s1 = 4 > 2 ? 'daugiau' : 'nedaugiau';
+console.log(s1);
+
+
+let s2 = '';
+if (4 < 2) {
+    s2 = 'daugiau';
+} else {
+    s2 = 'nedaugiau';
+}
+console.log(s2)
