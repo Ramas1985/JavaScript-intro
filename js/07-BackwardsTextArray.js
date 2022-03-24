56aae8095a11c3aa1c59451a8a7bb11d35775f28
Ramas1985 <ramunasskarnulis@gmail.com>
2022-03-24 19:50:57 +0100

    Backwards text reverse

---
+++ js/07-BackwardsTextArray.js
@@ -0,0 +1,25 @@
+/*
+Duotas array su tekstinemis reiksmemis (string)
+Reikia: sujungti juos atbulai iterpiant ", "
+*/
+
+const d = ['sakau', 'tau', 'rytas', 'labas'];
+let reverse = '';
+
+// reverse = reverse + d[3];
+// reverse = reverse + ', ';
+// reverse = reverse + d[2];
+// reverse = reverse + ', ';
+// reverse = reverse + d[1];
+// reverse = reverse + ', ';
+// reverse = reverse + d[0];
+// reverse = reverse + ', ';
+
+// console.log(reverse);
+
+// reverse = d[3] + ', ' + d[2] + ', ' + d[1] + ', ' + d[0];
+
+// console.log(reverse);
+
+reverse = `${d[3]}, ${d[2]}, ${d[1]}, ${d[0]}`;
+console.log(reverse);
